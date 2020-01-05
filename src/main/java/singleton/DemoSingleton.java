0f7f063780740af0e99ebcd65f21fb2f4866994e
yichunzhao <zhaoyichun@hotmail.com>
2020-01-05 15:22:54 +0100

    using enum to impl. singleton pattern

---
+++ src/main/java/singleton/DemoSingleton.java
@@ -0,0 +1,44 @@
+package singleton;
+
+/**
+ * when needing a singleton pattern? multiple threads access a common resource.
+ * <p>
+ * how to implementing singleton?
+ * method 1
+ * 1) private static instance, and private constructor
+ * 2) lazy loaded instance
+ * 3) complete or partial synchronize method
+ * <p>
+ * method 2
+ * using enum; an element of an enum is always single. enum can not be used to
+ * instantiate a new instance(private constructor).
+ */
+
+class BookingManager {
+
+    public void booking() {
+        System.out.println("manager is booking...");
+    }
+
+}
+
+enum BookingManagerSingleton {
+    INSTANCE;
+
+    public BookingManager bookingManager;
+
+    private BookingManagerSingleton() {
+        this.bookingManager = new BookingManager();
+    }
+
+}
+
+public class DemoSingleton {
+    public static void main(String[] args) {
+        BookingManager b1 = BookingManagerSingleton.INSTANCE.bookingManager;
+        BookingManager b2 = BookingManagerSingleton.INSTANCE.bookingManager;
+
+        if (b1 == b2) System.out.println("having the same reference");
+
+    }
+}
