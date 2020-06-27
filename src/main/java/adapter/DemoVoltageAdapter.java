9c279a33492a15776c4a16337ffbc9fa2efc8777
yichunzhao <zhaoyichun@hotmail.com>
2020-06-27 16:09:47 +0200

    comments for adapter

--- src/main/java/adapter/DemoVoltageAdapter.java
+++ src/main/java/adapter/DemoVoltageAdapter.java
@@ -1,5 +1,10 @@
 package adapter;
 
+/**
+ * Adapter have two interface and a concrete body Just like a real adapter.
+ * It is used to handle existing code without altering its structure and code, and reformatting its output or say
+ * behaviours to adapt to the client side structure or whatever input requirements.
+ */
 public class DemoVoltageAdapter {
 
     public static void main(String[] args) {
